c4905e5a02610f28e1a5c4f2dc4c771d023cc2b4
LamPLQ <lamplq131298@gmail.com>
2020-04-18 16:32:13 +0700

    fix display current time

--- src/main/java/com/edu/fpt/medtest/controller/FileController.java
+++ src/main/java/com/edu/fpt/medtest/controller/FileController.java
@@ -2,10 +2,12 @@ package com.edu.fpt.medtest.controller;
 
 import com.edu.fpt.medtest.model.UploadFileResponse;
 import com.edu.fpt.medtest.service.FileStorageService;
+import com.edu.fpt.medtest.utils.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.util.Base64;
 
 @RestController
 public class FileController {
@@ -36,31 +40,34 @@ public class FileController {
                 file.getContentType(), file.getSize());
     }
 
-    //save 1 file
-    @GetMapping("/saveFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
+    @PostMapping("/uploadImage")
+    public ResponseEntity uploadImage2(@RequestBody String base64String){
 
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
+        //decode string base64 to file image and copy link image
+        //imagePath
+        String[] strings = base64String.split(",");
+        String extension;
+        switch (strings[0]) {//check image's extension
+            case "data:image/jpeg;base64":
+                extension = "jpeg";
+                break;
+            case "data:image/png;base64":
+                extension = "png";
+                break;
+            default://should write cases for more images types
+                extension = "jpg";
+                break;
         }
-
-        // Fallback to the default content type if type could not be determined
-        if (contentType == null) {
-            contentType = "application/octet-stream";
+        //convert base64 string to binary data
+        byte[] data = DatatypeConverter.parseBase64Binary(strings[1]);
+        String path =  System.currentTimeMillis() + "_fileName." + extension;
+        File file = new File(path);
+        try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+            outputStream.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(resource.getFilename());
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                //.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+        return new ResponseEntity(file.getAbsolutePath(),HttpStatus.OK);
     }
-
 }
 
---
+++ src/main/java/com/edu/fpt/medtest/model/ImageFileUpload.java
@@ -0,0 +1,13 @@
+package com.edu.fpt.medtest.model;
+
+public class ImageFileUpload {
+    private String base64String;
+
+    public String getBase64String() {
+        return base64String;
+    }
+
+    public void setBase64String(String base64String) {
+        this.base64String = base64String;
+    }
+}
