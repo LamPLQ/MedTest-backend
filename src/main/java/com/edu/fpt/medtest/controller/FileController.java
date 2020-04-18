f17074fef6d17e817c7a97011e52fd8f40b58f48
LamPLQ <lamplq131298@gmail.com>
2020-04-19 00:24:42 +0700

    view image

--- src/main/java/com/edu/fpt/medtest/controller/FileController.java
+++ src/main/java/com/edu/fpt/medtest/controller/FileController.java
@@ -97,7 +97,7 @@ public class FileController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                //.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
 }
