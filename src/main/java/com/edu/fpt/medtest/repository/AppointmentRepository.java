ae473106c827faa15cff03bdf5e184f613786278
LamPLQ <lamplq131298@gmail.com>
2020-04-21 18:09:28 +0700

    delete comment

--- src/main/java/com/edu/fpt/medtest/repository/AppointmentRepository.java
+++ src/main/java/com/edu/fpt/medtest/repository/AppointmentRepository.java
@@ -2,13 +2,10 @@ package com.edu.fpt.medtest.repository;
 
 import com.edu.fpt.medtest.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Set;
 
 @Repository
 @Transactional
